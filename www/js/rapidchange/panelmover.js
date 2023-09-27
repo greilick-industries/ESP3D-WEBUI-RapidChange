471f32fe319f7e9ab01832d6424f5b8a1155c2cc
James Greilick <41349921+jgreilick@users.noreply.github.com>
2023-09-27 10:37:45 -0500

    added dust cover axis

--- www/js/rapidchange/panelmover.js
+++ www/js/rapidchange/panelmover.js
@@ -25,18 +25,10 @@ function wireEvents() {
 }
 
 function bringPanelsToRapidChange() {
-    // commandPanel.classList.add('rc-panel-max-height');
-    // controlsPanel.classList.add('rc-panel-max-height');
-    // grblPanel.classList.add('rc-panel-max-height');
-    // filesPanel.classList.add('rc-panel-max-height');
     rapidChangeParent.append(controlsPanel, commandPanel, grblPanel, filesPanel);
 }
 
 function sendPanelsToDashboard() {
-    // controlsPanel.classList.remove('rc-panel-max-height');
-    // commandPanel.classList.remove('rc-panel-max-height');
-    // grblPanel.classList.remove('rc-panel-max-height');
-    // filesPanel.classList.remove('rc-panel-max-height');
     dashboardParent.append(filesPanel);
     dashboardParent.insertBefore(grblPanel, id('temperaturesPanel'));
     dashboardParent.insertBefore(controlsPanel, grblPanel);
