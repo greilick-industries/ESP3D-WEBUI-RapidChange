a19bfdafc5966519bceb8d6d7ac5564cd3d47d57
James Greilick <41349921+jgreilick@users.noreply.github.com>
2024-08-12 12:55:44 -0500

    rework to revamp

--- www/js/rapidchange/panelcontrol.js
+++
@@ -1,8 +0,0 @@
-function togglePanel(btnId, panelId) {
-    const button = id(btnId);
-    const panel = id(panelId);
-    const wasShown = button.classList.toggle('btn-rc');
-    button.classList.toggle('btn-rc-orange');
-    panel.style.display = wasShown ? 'none' : 'flex';
-}
-
